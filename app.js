b5f403cbede267e73a75155ecdc2f1ccf03dcaa4
Rob <rob@Robs-MacBook-Pro.local>
2020-01-30 22:08:15 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+/* This is a quiz application with 5 questions
+ After each correct question a plpayer gets 1 point
+Player is rated with a ranking at end of the game. From 1st to 5th place
+ */
+document.write( < '<p> Hello test</p>');
